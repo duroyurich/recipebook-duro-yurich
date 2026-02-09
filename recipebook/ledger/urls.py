e0912b17ae8d4816274855f906d187adf91d12e3
immabutter <yurich.gabriel.duro@student.ateneo.edu>
2026-02-09 10:41:34 +0800

    Recipe List Template added

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import recipes_list, recipe_1, recipe_2
 
 urlpatterns = [
-    path('list', recipes_list, name='recipes-list'),
+    path('recipes/list', recipes_list, name='recipes_list'),
     path('recipe/1', recipe_1, name = 'recipe_1'),
     path('recipe/2', recipe_2, name = 'recipe_2')
 ]
