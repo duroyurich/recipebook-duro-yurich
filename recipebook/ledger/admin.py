e6999aa92e991710ed5504f20c984a9b9ea6a21a
immabutter <yurich.gabriel.duro@student.ateneo.edu>
2026-02-19 14:57:34 +0800

    Added IngredientAdmin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
-
-
-class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline,]
+from .models import Recipe, Ingredient, RecipeIngredient
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
 
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline,]
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
