87366374a2d35f5049c0a535796bc317e8f6f199
immabutter <yurich.gabriel.duro@student.ateneo.edu>
2026-02-19 14:40:58 +0800

    Updated urls, views and templates to use the models

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import recipes_list, recipe_1, recipe_2
+from .views import recipes_list, recipe
 
 urlpatterns = [
-    path('recipes/list', recipes_list, name='recipes_list'),
-    path('recipe/1', recipe_1, name='recipe_1'),
-    path('recipe/2', recipe_2, name='recipe_2')
+    path('recipes/list', recipes_list, name='recipes-list'),
+    path('recipe/<int:id>', recipe, name='recipe-detail'),
 ]
 
 app_name = 'ledger'
