254e999840e59a23564e4744650c1cf928228649
immabutter <yurich.gabriel.duro@student.ateneo.edu>
2026-03-12 17:03:07 +0800

    Added feature to create new recipes

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import recipes_list, recipe
+from .views import recipes_list, recipe, recipe_add, recipe_add_image
 
 urlpatterns = [
     path('recipes/list', recipes_list, name='recipes-list'),
     path('recipe/<int:id>', recipe, name='recipe-detail'),
+    path('recipe/add', recipe_add, name="recipe-add"),
+    path('recipe/<int:id>/add_image', recipe_add_image, name='recipe-add-image')
 ]
 
 app_name = 'ledger'
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Recipe
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import RecipeForm
 
 
 def recipes_list(request):
@@ -13,3 +15,20 @@ def recipes_list(request):
 def recipe(request, id):
     ctx = {'recipe': Recipe.objects.get(id=id)}
     return render(request, 'recipe.html', ctx)
+
+
+@login_required
+def recipe_add(request):
+    form = RecipeForm()
+    if request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid:
+            recipe = form.save()
+            return redirect('ledger:recipe-detail', id=recipe.id)
+    ctx = {"recipe": Recipe.objects.all(), "form": form}
+    return render(request, 'recipe_add.html', ctx)
+
+
+@login_required
+def recipe_add_image(request):
+    pass
