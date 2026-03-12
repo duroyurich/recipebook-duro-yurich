56e9f864a77c8d45193c1b2359328131a34738cd
immabutter <yurich.gabriel.duro@student.ateneo.edu>
2026-03-12 18:44:31 +0800

    Formatted to PEP8 standards

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('recipes/list', recipes_list, name='recipes-list'),
     path('recipe/<int:id>', recipe, name='recipe-detail'),
     path('recipe/add', recipe_add, name="recipe-add"),
-    path('recipe/<int:id>/add_image', recipe_add_image, name='recipe-add-image')
+    path('recipe/<int:id>/add_image', recipe_add_image,
+         name='recipe-add-image')
 ]
 
 app_name = 'ledger'
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Recipe
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
 from .forms import RecipeForm, ImageForm
 
 
